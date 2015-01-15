b59ede2cd670ae0faa5a999280d4b65a28d96da3
Gabor Szarnyas <szarnyasg@gmail.com>
2015-01-15 22:51:19 +0100

    Working RouteSensor and SwitchSensor cases

--- tb/PosLength.sh
+++ tb/PosLength.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ../bin/rdf3xload db railway-xform-1.ttl
-../bin/rdf3xquery db PosLength.sparql | tee | wc -l
+../bin/rdf3xquery db PosLength.sparql 2> /dev/null | tee /dev/tty | wc -l
---
+++ tb/RouteSensor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+time ../bin/rdf3xload db railway-xform-1.ttl
+
+time (
+../bin/rdf3xquery db RouteSensor.sparql 2> /dev/null | sort > r1.txt
+../bin/rdf3xquery db RouteSensor-neg.sparql 2> /dev/null | sort > r2.txt
+comm -23 r1.txt r2.txt > r.txt
+wc -l r.txt
+)
